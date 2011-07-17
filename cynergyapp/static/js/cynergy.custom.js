1c52f13efcfe67a067e24db6e341e0f9c03a2ec9
Chris Hunter <hunter@visibiz.com>
2011-07-17 18:23:04 -0400

    add inline edit

--- cynergyapp/static/js/cynergy.custom.js
+++ cynergyapp/static/js/cynergy.custom.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
-    
+    var lastSel;
     //Construct a jqGrid object
     $("#entries").jqGrid({
        	url:'/_get_entries',
     	datatype: "json",
        	colNames:['name', 'type', 'library','from_buss','to_buss', 'length', 'ampacity'],
        	colModel:[
-       		{name:'name',index:'name', width:90},
-       		{name:'type',index:'type', width:80, align:"right"},
-       		{name:'library',index:'library', width:80, align:"center"},		
-       		{name:'from_buss',index:'from_buss', width:80,align:"center"},		
-       		{name:'to_buss',index:'to_buss', width:80, sortable:false, align:"center"},
-       		{name:'length',index:'length', width:80,align:"center"},		
-       		{name:'ampacity',index:'ampacity', width:80, sortable:false,align:"center"}
+       		{name:'name',index:'name', width:90, editable: true, editoptions:{size:"20",maxlength:"50"}},
+       		{name:'type',index:'type', width:80, align:"right", editable: true, edittype:"select", editoptions:{value:"Type 1:Type 1;Type 2:Type 2;Type 3:Type 3"}},
+       		{name:'library',index:'library', width:80, editable: true, editoptions:{size:"20",maxlength:"50"}, align:"center"},		
+       		{name:'from_buss',index:'from_buss', width:80, editable: true, editoptions:{size:"20",maxlength:"50"}, align:"center"},		
+       		{name:'to_buss',index:'to_buss', width:80, sortable:false, editable: true, editoptions:{size:"20",maxlength:"50"}, align:"center"},
+       		{name:'length',index:'length', width:80, editable: true, editoptions:{size:"20",maxlength:"50"}, align:"center"},		
+       		{name:'ampacity',index:'ampacity', width:80, editable: true, editoptions:{size:"20",maxlength:"50"}, sortable:false,align:"center"}
        	],
        	rowNum:10,
        	rowList:[10,20,30],
@@ -22,7 +22,11 @@ $(document).ready(function(){
         width: '905',
         sortorder: "desc",
         caption:"Entries",
-        onSelectRow: function(id) {
+        onSelectRow: function(id){
+            if(id && id!==lastSel){ 
+               $('#entries').restoreRow(lastSel); 
+               lastSel=id; 
+            }
           $('#entries').editRow(id, true); 
         },
     });
